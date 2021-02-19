5d91148dab0b4b70d766554cdac099d61bef2b4c
anton-gabriel <anton.gabi1998@gmail.com>
2021-02-19 18:12:32 +0200

    Add client wrapper example.

---
+++ .NET Core 3.0/Greeting/GreetingClient/ClientEndpoint.cs
@@ -0,0 +1,27 @@
+﻿namespace GreetingClient
+{
+    using Generated;
+    using Grpc.Core;
+    using System;
+
+    internal sealed class ClientEndpoint
+        : IDisposable
+    {
+        public ClientEndpoint()
+        {
+            const string Host = "localhost";
+            const int Port = 50051;
+
+            this.channel = new Channel($"{Host}:{Port}", ChannelCredentials.Insecure);
+            Client = new GreetingService.GreetingServiceClient(channel);
+        }
+
+        private readonly Channel channel;
+        public GreetingService.GreetingServiceClient Client { get; }
+
+        public void Dispose()
+        {
+            this.channel.ShutdownAsync().Wait();
+        }
+    }
+}
--- .NET Core 3.0/Greeting/GreetingClient/Program.cs
+++ .NET Core 3.0/Greeting/GreetingClient/Program.cs
@@ -8,30 +8,24 @@ namespace GreetingClient
     {
         static void Main(string[] args)
         {
-            const string Host = "localhost";
-            const int Port = 50051;
-
-            var channel = new Channel($"{Host}:{Port}", ChannelCredentials.Insecure);
-            var client = new GreetingService.GreetingServiceClient(channel);
-
-            var request = new GreetingRequest
+           
+            using(var endpoint = new ClientEndpoint())
             {
-                Message = "Hi",
-                SenderName = "Marius"
-            };
+                var request = new GreetingRequest
+                {
+                    Message = "Hi",
+                    SenderName = "Gabi"
+                };
 
-            Console.WriteLine("Client sending request: ");
-            Console.WriteLine($"{request} {System.Environment.NewLine}");
+                Console.WriteLine("Client sending request: ");
+                Console.WriteLine($"{request} {System.Environment.NewLine}");
 
-            using (var response = client.GreetAsync(request))
-            {
+
+                using var response = endpoint.Client.GreetAsync(request);
                 Console.WriteLine("Client received response: ");
                 Console.WriteLine(response.ResponseAsync.Result);
             }
 
-
-            // Shutdown
-            channel.ShutdownAsync().Wait();
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
